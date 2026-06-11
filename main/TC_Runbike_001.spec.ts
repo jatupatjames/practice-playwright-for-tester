a81c902e6c6d5ebf16cac9e897b2fc078e94376d
Diamond <sarunyoochimmalee@gmail.com>
2026-06-11 17:15:12 +0700

    add comment //test1234

--- main/TC_Runbike_001.spec.ts
+++ main/TC_Runbike_001.spec.ts
@@ -3,6 +3,7 @@ import { login } from '../action/auth';
 import { loginInfo } from '../data/auth';
 
 //test
+//test1234
 
 test('Login', async ({ page }) => {
   await login(page, loginInfo.jamesUsername, loginInfo.jamesPassword);

96465eb5f788b65e8d0aa3efd7badf96eca1747c
toeypnptp <tonpum_p@silpakorn.edu>
2026-06-12 14:51:15 +0700

    delete comment

--- main/TC_Runbike_001.spec.ts
+++ main/TC_Runbike_001.spec.ts
@@ -2,8 +2,7 @@ import { test, expect } from '@playwright/test';
 import { selectCheckbox } from '../action/Actions';
 import { checkboxName } from '../data/Actions';
 
-//test
-//test1234
+// toey
 
 test('Function Checkbox', async ({ page }) => {
   await page.goto('https://runbike-event.web.app/login');

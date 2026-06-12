b7ed624c1e502b801a0fd7d226c3f679698b5ae8
James <jatupat.ji@gmail.com>
2026-06-12 14:38:37 +0700

    add checkbox function

--- action/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Page ,expect } from '@playwright/test';
-
-export async function login(page: Page, username: string, password: string) {
-  await page.goto('https://runbike-event.web.app/login');
-  await page.getByLabel('อีเมลหรือเบอร์โทรศัพท์').fill(username);
-  await page.getByLabel('รหัสผ่าน').fill(password);
-  await page.locator('button[type="submit"]', { hasText: 'เข้าสู่ระบบ' }).click();
-  await expect(page.getByRole('heading',{name: 'ประวัติการลงทะเบียน'})).toBeVisible();
-}
---
+++ data/Actions.ts
@@ -0,0 +1,10 @@
+import { Page } from '@playwright/test';
+
+export interface Checkbox {
+    AnnualMember : string
+    }
+
+export const checkboxName:Checkbox = {
+AnnualMember: '#racerList_0_annualMember'
+}
+

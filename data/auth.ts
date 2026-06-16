e9a4bf29d46000738f4099c741a7150a4d616dcc
Diamond <sarunyoochimmalee@gmail.com>
2026-06-16 15:27:14 +0700

    เพิ่มตัวแปร resetPassword

--- data/auth.ts
+++ data/auth.ts
@@ -1,12 +1,22 @@
 import { Page } from '@playwright/test';
+import { Client } from 'pg';
 
 export interface Login {
     jamesUsername : string,
     jamesPassword : string
     }
 
-export const loginInfo:Login = {
-jamesUsername : '0888614919',
-jamesPassword : 'P@ssw0rd',
+export interface ResetPassword {
+    forgetEmail: string, 
+    newPassword: string,
+    }
+
+export const loginInfo: Login = {
+  jamesUsername: '0888614919',
+  jamesPassword: 'P@ssw0rd',
 }
 
+export const forgetInfo: ResetPassword = {
+  forgetEmail: 'sarunyoochimmalee@gmail.com',
+  newPassword: 'ABC456',
+}

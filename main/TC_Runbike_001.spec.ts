2d6cd32187076fc75b37d750c9bf8c082261230a
James <jatupat.ji@gmail.com>
2026-06-17 17:16:00 +0700

    Call function assertLoginPage and scrollModal in file checkbox.ts and delete some comments

--- main/TC_Runbike_001.spec.ts
+++ main/TC_Runbike_001.spec.ts
@@ -3,14 +3,10 @@ import { forgetPassword, login } from '../action/auth';
 import { loginInfo } from '../data/auth';
 import { getOtpFromDb } from '../data/db';
 
-//test
-//test1234
-
 test.skip('Login', async ({ page }) => {
   await login(page, loginInfo.jamesUsername, loginInfo.jamesPassword);  
 });
 
 test('Forgot Password', async ({ page }) => {
   await forgetPassword(page, 'sarunyoochimmalee@gmail.com', 'ABC777');
-  //test pull request
 });

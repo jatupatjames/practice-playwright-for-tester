912a68973b1e070d4f2b9bab5073ba11b3173c75
aungaing2544 <aungaing2544@gmail.com>
2026-06-17 16:58:15 +0700

    create action scrollmodal

--- action/Actions.ts
+++ action/Actions.ts
@@ -12,3 +12,31 @@ export async function inputValue(locator, value) {
   }
 
   //Benz
+
+export async function scrollModal(page: Page) {
+    // Locator ของ Modal ที่แสดงเนื้อหานโยบาย, Locator ของปุ่ม "รับทราบและยอมรับ", ดึงตำแหน่งและขนาดของ Modal
+    const modal = page.locator('.ant-modal-body');
+    const acceptButton = page.getByRole('button', {name: 'รับทราบและยอมรับ'});
+    const box = await modal.boundingBox();
+
+    // เงื่อนไขกรณีไม่เจอ Modal ให้ Throw Error และหยุดการทำงาน
+    if (!box) {
+        throw new Error('Modal not found');
+    }
+
+    // คำสั่งย้ายเมาส์ไปยังกลาง Modal เพื่อให้ Mouse Scroll เฉพาะภายใน Modal
+    await page.mouse.move(
+        box.x + box.width / 2,
+        box.y + box.height / 2
+    );
+
+    // คำสั่งให้ Scroll ลงเรื่อยๆจนกว่าปุ่ม "รับทราบและยอมรับ" จะ Enable
+    while (!(await acceptButton.isEnabled())) {
+
+        // Scroll ลงทีละ 500 pixel
+        await page.mouse.wheel(0, 2000);
+
+        // รอให้ UI อัปเดตสถานะปุ่ม
+        await page.waitForTimeout(100);
+    }
+}

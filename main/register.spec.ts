fe45a7c81841e9dea704d241aa5dab46a6d27e8e
minial2541 <minial2541@gmail.com>
2026-06-18 18:15:46 +0700

    register update data

--- main/register.spec.ts
+++ main/register.spec.ts
@@ -7,11 +7,11 @@ test('Function Register', async ({ page }) => {
   await page.goto('https://runbike-event.web.app/login'); 
 
   await page.getByTitle('สมัครสมาชิก').click();
-  await page.locator('#username').fill('Aunthika');
-  await page.locator('#mobileNo').fill('0842746696');
-  await page.locator('#email').fill('aunthika.wo@gmail.com');
-  await page.locator('#password').fill('P@ssw0rd');
-  await page.locator('#confirmPassword').fill('P@ssw0rd');
+  await page.locator('#username').fill('Panupong');
+  await page.locator('#mobileNo').fill('0889818509');
+  await page.locator('#email').fill('panupong.pnptp@gmail.com');
+  await page.locator('#password').fill('P@ssw0rd013');
+  await page.locator('#confirmPassword').fill('P@ssw0rd013');
   await page.click('button[type="submit"]');
 
   

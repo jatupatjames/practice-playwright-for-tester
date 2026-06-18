7f537be0f41c68180bafee80dae09698a94024aa
minial2541 <minial2541@gmail.com>
2026-06-18 18:32:35 +0700

    register update test

--- main/register.spec.ts
+++ main/register.spec.ts
@@ -7,7 +7,7 @@ test('Function Register', async ({ page }) => {
   await page.goto('https://runbike-event.web.app/login'); 
 
   await page.getByTitle('สมัครสมาชิก').click();
-  await page.locator('#username').fill('Panupong');
+  await page.locator('#username').fill('Panupong Test');
   await page.locator('#mobileNo').fill('0889818509');
   await page.locator('#email').fill('panupong.pnptp@gmail.com');
   await page.locator('#password').fill('P@ssw0rd013');

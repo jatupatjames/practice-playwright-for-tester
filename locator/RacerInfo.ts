2853b4f2cb9077bea893978657b272099011dd4c
Diamond <sarunyoochimmalee@gmail.com>
2026-07-01 12:04:04 +0700

    add racerInfo2

--- locator/RacerInfo.ts
+++ locator/RacerInfo.ts
@@ -5,6 +5,12 @@ export interface field {
     enLastName : string,
     nickname : string,
     country : string,
+    thFirstName2 : string,
+    thLastName2 : string,
+    enFirstName2 : string,
+    enLastName2 : string,
+    nickname2 : string,
+    country2 : string,
     }
 
 export const racerInfo:field = {
@@ -14,5 +20,10 @@ export const racerInfo:field = {
     enLastName : '#racerList_0_lastnameEn',
     nickname : '#racerList_0_nickname',
     country : 'racerList_0_nationality',
-
+    thFirstName2: '#racerList_1_firstnameTh',
+    thLastName2: '#racerList_1_lastnameTh',
+    enFirstName2 : '#racerList_1_firstnameEn',
+    enLastName2 : '#racerList_1_lastnameEn',
+    nickname2 : '#racerList_1_nickname',
+    country2 : 'racerList_1_nationality',
 }

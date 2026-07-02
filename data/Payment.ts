f5dc3505b0b3da57d2cc0453293e0e96f76e398c
James <jatupat.ji@gmail.com>
2026-07-02 16:53:13 +0700

    add steps until payment screen

---
+++ data/Payment.ts
@@ -0,0 +1,15 @@
+export interface Payment {
+    cardNo : string,
+    expDate : string,
+    cvv : string,
+    nameOnCard : string
+}
+
+export const cardInfo:Payment = {
+cardNo: '4111111111111111',
+expDate: '0930',
+cvv: '123',
+nameOnCard: 'Jatupat Jirapraphai'
+
+
+}
